65f6a562f49f06e48eaa013de8e8e4047df17e81
Euan <40454766@live.napier.ac.uk>
2021-06-21 19:25:54 +0100

    Added sensor to get acceleration and speed.

--- app/src/main/java/com/example/phonethrowapp/MainActivity.java
+++ app/src/main/java/com/example/phonethrowapp/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.phonethrowapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private long time;
+    private float high_score = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        time = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            getAccelerometer(event);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private void getAccelerometer(SensorEvent event){
+        float[] values = event.values;
+        TextView speedText = findViewById(R.id.txtSpeed);
+        TextView highScoreText = findViewById(R.id.txtHighScore);
+
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float speed = x + y + z;
+        speedText.setText((int) speed + "m/s");
+
+        if(speed > high_score)
+        {
+            high_score = speed;
+            highScoreText.setText((int) high_score + "m/s");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
